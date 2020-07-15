14b9c0974cc86bae9a942d3762fc594816fc4d33
gagan <sherry2312.gk@gmail.com>
2020-07-15 15:55:54 -0400

    declaring the variables

---
+++ app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/LocationActivity.java
@@ -0,0 +1,14 @@
+package com.example.tovisit_gagandeepkaur_768688_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
--- app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/MainActivity.java
+++ app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.tovisit_gagandeepkaur_768688_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Button addPlace;
+    SwipeMenuListView locationListView;
 
 
 
@@ -17,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addPlace = findViewById(R.id.btn_add_place);
+
+        locationListView =  findViewById(R.id.location_list_view);
+
+        addPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LocationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
