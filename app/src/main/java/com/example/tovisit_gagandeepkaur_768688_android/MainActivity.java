8b6ba3fba20025229099e4f7e1a01e293bd45520
gagan <sherry2312.gk@gmail.com>
2020-07-15 15:53:20 -0400

    adding layout

--- app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/MainActivity.java
+++ app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.tovisit_gagandeepkaur_768688_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
