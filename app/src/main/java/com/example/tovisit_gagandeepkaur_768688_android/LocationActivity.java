e0d35598a3a5c8cbd3822a652844046d63a62045
gagandeep kaur <sherry2312.gk@gmail.com>
2020-07-19 18:48:40 -0400

    adding googlemap

--- app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/LocationActivity.java
+++ app/src/main/java/com/example/tovisit_gagandeepkaur_768688_android/LocationActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LocationActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
+
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
